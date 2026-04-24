51160615621be5f78cfb2815ea156baa60733ed1
Nitin <rajnitin793@gmail.com>
2026-04-24 12:32:28 +0530

    added resources for game and convert into an object

---
+++ src/images/images.js
@@ -0,0 +1,15 @@
+import dirt from './dirt.jpg'
+import glass from './glass.png'
+import grass from './grass.jpg'
+import log from './log.jpg'
+import wood from './wood.png'
+
+const images = {
+    dirt,
+    glass,
+    grass,
+    log,
+    wood
+}
+
+export default images;
