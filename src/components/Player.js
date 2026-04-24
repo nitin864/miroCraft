cb9bdfe99bc54a2657f7d3cdb7564ca3a41c0d20
Nitin <rajnitin793@gmail.com>
2026-04-24 20:58:59 +0530

    integrated jump movement in player

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,11 +4,12 @@ import { useEffect, useRef } from "react"
 import { Vector2, Vector3 } from "three"
 import { useKeyboard } from "../hooks/useKeyboard"
  
+const jump_force = 5
 
 export const Player = () => {
    
     const actions  =  useKeyboard()
-    console.log('actions', Object.entries(actions).filter(([k,v]) => v))
+     
   
     const { camera } = useThree()
     useEffect(() => {
@@ -34,6 +35,10 @@ export const Player = () => {
     useFrame(() => {
         
 		camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
+
+        if(actions.jump && Math.abs(vel.current[1]) < 0.05){
+            api.velocity.set(vel.current[0], jump_force,vel.current[0])
+        }
     })
 
     return(
